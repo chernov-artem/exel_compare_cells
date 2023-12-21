5b35532fa4a2dc0579214f32173de81f76699fa4
Artem <spb9719268@yandex.ru>
2023-12-21 21:35:55 +0300

    добавлена функция сравнения ячеек

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 import re
 from openpyxl import load_workbook
 
+""" Работа с exel файлами
+Сравнивает 2 ячейки exel. Находитв первой ячейки модель товара и ищет в другой ячейке такую же модель. Пробелы и точки игнорируются.
+
+"""
+
+def compare_cells(num: int) -> bool:
+    "получает на вход номер ряда и сравнивает ячейки B и C в этом ряду. Возвращает True в случае совпадения"
+    num_cell_B = 'B' + str(num)
+    num_cell_C = 'C' + str(num)
+    tmp1 = ws[num_cell_B].value
+    tmp2 = ws[num_cell_C].value
+    print(tmp1)
+    print(tmp2)
+    if tmp1 != [] and tmp2 != []:
+        try:
+            print(tmp1.split(' ')[0], tmp2.split(' ')[0])
+        except Exception as ex:
+            print("ex = ", ex)
+
+wb = load_workbook('sber18122023.xlsx')
+ws = wb['Аня']
+
+
 if __name__ == '__main__':
+    compare_cells(1198)
